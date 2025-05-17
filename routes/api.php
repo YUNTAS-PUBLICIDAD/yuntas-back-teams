800671be1d7af96542aeb465e3ac501e9df17610
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2025-05-17 11:02:06 -0500

    add documentacion de edtiar productos

--- routes/api.php
+++ routes/api.php
@@ -130,6 +130,7 @@ Route::prefix("v2")->group(function(){
     Route::controller(V2ProductoController::class)->prefix("/productos")->group(function(){
         Route::get("/", "index");
         Route::post("/", "store");
+        Route::put("/{id}", "update");
         Route::delete("/{id}", "destroy")->whereNumber("id");
     });
 });
