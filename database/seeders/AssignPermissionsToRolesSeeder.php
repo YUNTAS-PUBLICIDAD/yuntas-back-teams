b670fa123ad666175080615d22375ee14f110d3a
Erick ZG <ezavaletag@unitru.edu.pe>
2025-05-27 11:12:38 -0500

    arreglo de seeder para asignar permisos

--- database/seeders/AssignPermissionsToRolesSeeder.php
+++ database/seeders/AssignPermissionsToRolesSeeder.php
@@ -13,9 +13,39 @@ class AssignPermissionsToRolesSeeder extends Seeder
         $adminRole = Role::where('name', 'admin')->first();
 
         $permissions = Permission::whereIn('name', [
+            // Gestión general
             'gestionar-roles',
             'gestionar-permisos',
             'asignar-permisos-roles',
+            'asignar-roles-usuarios',
+
+            // Usuarios
+            'ver-usuarios',
+            'crear-usuarios',
+            'editar-usuarios',
+            'eliminar-usuarios',
+
+            // Clientes
+            'ver-clientes',
+            'crear-clientes',
+            'editar-clientes',
+            'eliminar-clientes',
+
+            // Reclamos
+            'ver-reclamos',
+            'crear-reclamos',
+            'editar-reclamos',
+            'eliminar-reclamos',
+
+            // Blogs
+            'crear-blogs',
+            'editar-blogs',
+            'eliminar-blogs',
+
+            // Tarjetas
+            'crear-tarjetas',
+            'editar-tarjetas',
+            'eliminar-tarjetas',
         ])->pluck('id');
 
         $adminRole->syncPermissions($permissions);
