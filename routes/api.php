9ebe16d1b1ea776aca2d91dae9207264b4cb6792
iguevarav <iguevara@unitru.edu.pe>
2025-06-05 11:14:26 -0500

    Actualizacion de Blogs

--- routes/api.php
+++ routes/api.php
@@ -23,20 +23,18 @@ use Illuminate\Support\Facades\Auth;
 
 // blogs públicos
 Route::get('/cards', [CardController::class, "index"]);
-Route::get('/blogs/{id}', [BlogController::class, "show"]);
+
 Route::get('/blogs', [BlogController::class, "index"]);
-Route::get('/blog_head/{id}', [BlogHeadController::class, "show"]);
-Route::get('/blog_footer/{id}', [BlogFooterController::class, "show"]);
-Route::get('/blog_body/{id}', [BlogBodyController::class, "show"]);
+Route::get('/blogs/{id}', [BlogController::class, "show"]);
 Route::get('/blogs/link/{link}', [BlogController::class, "getByLink"]);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('permission:crear-blogs')->post('/blogs', [BlogController::class, "store"]);
+    Route::middleware('permission:editar-blogs')->put('/blog/{id}', [BlogController::class, "update"]);
+    Route::middleware('permission:eliminar-blogs')->delete('/blogs/{id}', [BlogController::class, "destroy"]);
+
     //rutas create blog
     Route::middleware('permission:crear-blogs')->post('/card', [CardController::class, "create"]);
-    Route::middleware('permission:crear-blogs')->post('/blogs', [BlogController::class, "create"]);
-    Route::middleware('permission:crear-blogs')->post('/blog_head', [BlogHeadController::class, "create"]);
-    Route::middleware('permission:crear-blogs')->post('/blog_body', [BlogBodyController::class, "create"]);
-    Route::middleware('permission:crear-blogs')->post('/blog_footer', [BlogFooterController::class, "create"]);
     Route::middleware('permission:crear-tarjetas')->post('/commend_tarjeta', [CommendTarjetaController::class, "create"]);
     Route::middleware('permission:crear-tarjetas')->post('/tarjeta', [TarjetaController::class, "create"]);
     Route::middleware('permission:crear-tarjetas')->post('/card/blog/image_head/{id}', [CardController::class, "imageHeader"]);
@@ -45,19 +43,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
     //rutas update blog
     Route::middleware('permission:editar-blogs')->put('/card/{id}', [CardController::class, "update"]);
-    Route::middleware('permission:editar-blogs')->put('/blog/{id}', [BlogController::class, "update"]);
-    Route::middleware('permission:editar-blogs')->put('/blog_head/{id}', [BlogHeadController::class, "update"]);
-    Route::middleware('permission:editar-blogs')->put('/blog_body/{id}', [BlogBodyController::class, "update"]);
-    Route::middleware('permission:editar-blogs')->put('/blog_footer/{id}', [BlogFooterController::class, "update"]);
     Route::middleware('permission:editar-blogs')->put('/commend_tarjeta/{id}', [CommendTarjetaController::class, "update"]);
     Route::middleware('permission:editar-blogs')->put('/tarjeta/{id}', [TarjetaController::class, "update"]);
 
     //rutas delete blog
     Route::middleware('permission:eliminar-blogs')->delete('/cards/{id}', [CardController::class, "destroy"]);
-    Route::middleware('permission:eliminar-blogs')->delete('/blogs/{id}', [BlogController::class, "destroy"]);
-    Route::middleware('permission:eliminar-blogs')->delete('/blog_head/{id}', [BlogHeadController::class, "destroy"]);
-    Route::middleware('permission:eliminar-blogs')->delete('/blog_body/{id}', [BlogBodyController::class, "destroy"]);
-    Route::middleware('permission:eliminar-blogs')->delete('/blog_footer/{id}', [BlogFooterController::class, "destroy"]);
     Route::middleware('permission:eliminar-tarjetas')->delete('/commend_tarjeta/{id}', [CommendTarjetaController::class, "destroy"]);
     Route::middleware('permission:eliminar-tarjetas')->delete('/tarjetas_delete/{id}', [TarjetaController::class, "destroyAll"]);
 
