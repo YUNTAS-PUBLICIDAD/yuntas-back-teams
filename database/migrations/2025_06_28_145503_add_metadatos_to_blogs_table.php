12f2b93394982a4d7c569c5657adb8fcc9b4ecca
iguevarav <iguevara@unitru.edu.pe>
2025-06-28 10:34:48 -0500

    Migracion para metadatos en blogs

---
+++ database/migrations/2025_06_28_145503_add_metadatos_to_blogs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('meta_titulo')->nullable()->unique()->after('imagen_principal');
+            $table->string('meta_descripcion')->nullable()->after('meta_titulo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('meta_titulo');
+            $table->dropColumn('meta_descripcion');});
+    }
+};
