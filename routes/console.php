76f505443e7e037279cb2f81f6d2937d53f1645f
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-09-16 16:26:16 -0500

    chore: ejecutar command cada minuto

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\EnviarPrimeraPublicidad;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-Schedule::command(EnviarPrimeraPublicidad::class)->timezone('America/Lima')->cron('*/2 * * * *');
+Schedule::command(EnviarPrimeraPublicidad::class)->timezone('America/Lima')->cron('*/1 * * * *');
