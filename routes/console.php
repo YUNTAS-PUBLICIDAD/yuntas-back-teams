5f4a926f3a7df1495383032d0b030cebd74afc78
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-09-18 11:19:06 -0500

    feat: agregar segundo comando de emails

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Console\Commands\SendFirstPromotionalEmail;
+use App\Console\Commands\SendSecondPromotionalEmail;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(SendFirstPromotionalEmail::class)->timezone('America/Lima')->cron('*/1 * * * *');
+
+Schedule::command(SendSecondPromotionalEmail::class)->timezone('America/Lima')->cron('*/1 * * * *');
