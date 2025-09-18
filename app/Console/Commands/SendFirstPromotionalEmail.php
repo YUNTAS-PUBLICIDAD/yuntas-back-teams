112a1b9d8c36a166992ae24b925dd0c8a5170278
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-09-18 11:18:03 -0500

    refactor: optimizar consulta para evitar lentittud

--- app/Console/Commands/SendFirstPromotionalEmail.php
+++ app/Console/Commands/SendFirstPromotionalEmail.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 
 class SendFirstPromotionalEmail extends Command
 {
-    protected $signature = 'app:enviar-primera-publicidad';
+    protected $signature = 'app:send-first-promotional-email';
 
     protected $description = 'Envio de publicidad luego de consultar un producto por medio del popup';
 
@@ -19,25 +19,25 @@ class SendFirstPromotionalEmail extends Command
     {
         $now = Carbon::now('America/Lima');
 
-        $clientes = Cliente::all();
-
         $views = [
             'paneles-led-electronicos' => 'emails.prueba.prueba3',
             'letreros-acrilicos' => 'emails.prueba.prueba3',
         ];
 
+        $clientes = Cliente::with('producto')
+            ->whereRaw("DATE_FORMAT(created_at, '%Y-%m-%d %H:%i') = ?", [$now->subMinutes(2)->format('Y-m-d H:i')])
+            ->get();
+
         foreach ($clientes as $cliente) {
-            $created_at = Carbon::parse($cliente->created_at);
+            $view = $views[$cliente->producto->link] ?? null;
 
-            $targetTime = $created_at->addMinutes(2);
-
-            $producto = Producto::findOrFail($cliente->producto_id);
-
-            $view = $views[$producto->link] ?? null;
-
-            if ($now->format('Y-m-d H:i') == $targetTime->format('Y-m-d H:i')) {
-                SendPromotionalEmailJob::dispatch($cliente->email, $view, $cliente->name);
-                Log::info('Publicidad para el cliente' . $cliente->name . ' enviada al job.');
+            if ($view) {
+                SendPromotionalEmailJob::dispatch(
+                    $cliente->email,
+                    $view,
+                    $cliente->name
+                );
+                Log::info("Publicidad enviada al cliente {$cliente->name}");
             }
         }
     }
