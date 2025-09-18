64cf1c211f207e57ceb4d5032e15a8519693d389
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-09-18 11:18:24 -0500

    refactor: quitar instancia innecesario de producto

--- app/Console/Commands/SendFirstPromotionalEmail.php
+++ app/Console/Commands/SendFirstPromotionalEmail.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use App\Jobs\SendPromotionalEmailJob;
 use App\Models\Cliente;
-use App\Models\Producto;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
