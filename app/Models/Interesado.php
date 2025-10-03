24880079fb57a3e33803b7db702404dcb8f26914
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-10-03 17:52:27 -0500

    mailing automatizado y creación de la base de datos

--- app/Models/Interesado.php
+++ app/Models/Interesado.php
@@ -11,10 +11,12 @@ class Interesado extends Model
     use HasFactory;
 
     protected $fillable = [
-        'nombre',
-        'email',
-        'cliente_id',
-        'producto_id'
+         'cliente_id',
+        'producto_id',
+        'titulo',
+        'parrafo1',
+        'imagen_prin',
+        'imagenes_sec',
     ];
 
     public function cliente(): BelongsTo
