1c71a0a58faf31dd06eee85eca06dfcec20a1e13
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-10-06 19:25:06 -0500

    feat: crear funcion para concatenar la api url con las imagenes

--- app/Models/EmailProducto.php
+++ app/Models/EmailProducto.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailProducto extends Model
 {
+    public static $apiUrl = 'https://apiyuntas.yuntaspublicidad.com';
+
     protected $fillable = [
         'producto_id',
         'titulo',
@@ -18,4 +20,16 @@ class EmailProducto extends Model
     {
         return $this->belongsTo(Producto::class);
     }
+
+    public static function buildImageUrl($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        return preg_match('/^https?:\/\//', $value)
+            ? $value
+            : self::$apiUrl . $value;
+    }
+
 }
