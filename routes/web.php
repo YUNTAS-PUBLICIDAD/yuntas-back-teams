d63717005902b0247715cd52483fa0f0486da00c
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-10-09 11:49:23 -0500

    feat: agregar endpoint para enlazar el storage

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ExportController;
-
-use App\Http\Controllers\Api\V1\Productos\ProductoController; 
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\Api\V1\Productos\ProductoController;
 
 Route::get('/exportProducto', [ExportController::class, 'exportProducto']);
 Route::get('/exportBlog', [ExportController::class, 'exportBlog']);
@@ -15,3 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('productos', ProductoController::class);
+
+Route::get("/generate-link-simbolink", function(){
+    if(file_exists(public_path("storage"))){
+        return "existe";
+    }
+
+    Artisan::call("storage:link");
+    app('files')->link(storage_path("app/public"), public_path("storage"));
+    return "creado";
+});
